4afd7a77356c686245347bd36f0f1f252b9734f0
Softwarica College Lab <softwarica@softwarica.com>
2019-04-11 11:40:39 +0545

    Select Time Format

--- app/src/main/java/com/e/androiddatetimepicker/MainActivity.java
+++ app/src/main/java/com/e/androiddatetimepicker/MainActivity.java
@@ -1,11 +1,13 @@
 package com.e.androiddatetimepicker;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -14,6 +16,7 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     private EditText tvDOB;
+    private EditText tvTime;
 
     @Override
 
@@ -22,13 +25,20 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         setContentView(R.layout.activity_main);
 
         tvDOB = findViewById(R.id.tvDOB);
+        tvTime = findViewById(R.id.tvTime);
+
         tvDOB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loadDatePicker();
-
             }
+        });
 
+        tvTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadTime();
+            }
         });
 
     }
@@ -49,7 +59,21 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
 
     }
 
+    private void loadTime() {
+        final Calendar c = Calendar.getInstance();
+        final int hour = c.get(Calendar.HOUR);
+        int minute = c.get(Calendar.MINUTE);
+        int second = c.get(Calendar.SECOND);
 
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                tvTime.setText("Time is" + hourOfDay + ";" + minute);
+            }
+        }, hour, minute, false);
+            timePickerDialog.show();
+    }
 
 
     @Override
